67c83779a7c1d46d9460627242b538e93906a9d3
Igor Malovitsa <igor.mlvts@gmail.com>
2020-06-20 15:47:18 +0300

    added matrix decomposition functions

--- src/matrix.js
+++ src/matrix.js
@@ -1,3 +1,14 @@
+const vecLength = (vec, len) => {
+    if (!len) {
+        len = vec.length;
+    }
+    let sum = 0;
+    for (let i = 0; i < len; i++) {
+        sum += vec[i] * vec[i];
+    }
+    return Math.sqrt(sum);
+};
+
 export class Matrix4 {
     constructor(data) {
         this.data = data;
@@ -96,5 +107,48 @@ export class Matrix4 {
             out[i] += data[i][3];
         }
         return out;
+    }
+
+    /**
+     * Decopose a matrix into translation; scale; rotation
+     */
+    decompose() {
+        const data = this.data;
+        const translation = [data[0][3], data[1][3], data[2][3]];
+        const scale = [
+           vecLength(data[0], 3),
+           vecLength(data[1], 3),
+           vecLength(data[2], 3),
+        ];
+        const rotation = Matrix4.unit();
+        const rotData = rotation.data;
+        for (let row = 0; row < 3; row++) {
+            for (let col = 0; col < 3; col++) {
+                rotData[row][col] = data[row][col] / scale[row];
+            }
+        }
+        return {
+            translation,
+            scale,
+            rotation,
+        };
+    }
+    toEulerAngles() {
+        // assert isRotation
+        const data = this.data;
+        const sy = Math.sqrt(data[0][0] * data[0][0] + data[1][0] * data[1][0]);
+        if (sy > 1e-6) {
+            return [
+               Math.atan2(data[2][1], data[2][2]),
+               Math.atan2(-data[2][0], sy),
+               Math.atan2(data[1][0], data[0][0]),
+            ];
+        } else {
+            return [
+                Math.atan2(-data[1][2], data[1][1]),
+                Math.atan2(-data[2][0], sy),
+               0,
+            ];
+        }
     }
 }
